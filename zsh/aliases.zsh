2e03d27b0cff9fb694823811673aed5c143f1267
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-10-09 18:09:29 +0200

    colorize ll, restart for pow, ctags

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,13 @@
 ### ls aliases
-alias ll='ls -halF'
-alias la='ls -A'
-alias l='ls -CF'
+alias ll='ls -GhalF'
+alias la='ls -GA'
+alias l='ls -GCF'
 
 ### spring aliases
 alias srails='spring rails'
 alias srake='spring rake'
 alias srspec='spring rspec'
-
+alias restart='touch ~/.pow/restart.txt'
 
 ### git aliases
 # add all modified
@@ -24,3 +24,4 @@ alias kill='kill -9' # always kill to death
 
 ### Not used/assorted
 alias bc="bc -lq"
+alias gctags='ctags -R --languages=ruby --exclude=.git --exclude=log --exclude=tmp . $(bundle list --paths)'
